9efbd6ec38e838f51543708a5c18fcc573367214
rhong99 <ryan.hong99@proton.me>
2022-10-12 21:45:15 -0400

    Refresh

--- Sidekick/User/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
-
-def home(request):
-    context = {
-        'title': 'Home Page'
-    }
-    return render(request, 'User/home.html', context)
--- Sidekick/manage.py
+++ Sidekick/manage.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sidekick.Sidekick.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sidekick.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
